022a687db0316a93d80916a02ddfc61445de9367
JT Archie <jarchie@pivotal.io>
2018-02-27 18:14:39 -0700

    persist to a local db

--- pkg/writers/web/server.go
+++ pkg/writers/web/server.go
@@ -2,12 +2,15 @@ package writers
 
 import (
 	"fmt"
+	"html"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"strconv"
 	"time"
 
 	"github.com/blevesearch/bleve"
+	bolt "github.com/boltdb/bolt"
 	"github.com/jtarchie/syslog/pkg/log"
 )
 
@@ -15,6 +18,7 @@ type Server struct {
 	port       int
 	index      bleve.Index
 	httpServer *http.Server
+	db         *bolt.DB
 }
 
 type doc struct {
@@ -29,15 +33,40 @@ type doc struct {
 }
 
 func NewServer(port int) *Server {
+	index, err := bleve.NewMemOnly(bleve.NewIndexMapping())
+	if err != nil {
+		log.Fatalf("could not start indexer: %s", err)
+	}
+
+	tmpFile, err := ioutil.TempFile("", "messages")
+	if err != nil {
+		log.Fatalf("could not create db: %s", err)
+	}
+
+	db, err := bolt.Open(tmpFile.Name(), 0600, &bolt.Options{Timeout: 10 * time.Second})
+	if err != nil {
+		log.Fatalf("could not start db: %s", err)
+	}
+
+	db.Update(func(tx *bolt.Tx) error {
+		_, err := tx.CreateBucketIfNotExists([]byte("messages"))
+		if err != nil {
+			return fmt.Errorf("create bucket: %s", err)
+		}
+		return nil
+	})
+
 	return &Server{
-		port: port,
+		index: index,
+		port:  port,
+		db:    db,
 	}
 }
 
 func (s *Server) Write(l *syslog.Log) error {
 	id := strconv.FormatInt(time.Now().UnixNano(), 10)
 
-	return s.index.Index(id, doc{
+	err := s.index.Index(id, doc{
 		Version:   l.Version(),
 		Priority:  l.Priority(),
 		Timestamp: l.Timestamp(),
@@ -47,38 +76,54 @@ func (s *Server) Write(l *syslog.Log) error {
 		MsgID:     l.MsgID(),
 		Message:   l.Message(),
 	})
+
+	if err != nil {
+		return err
+	}
+
+	return s.db.Update(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket([]byte("messages"))
+		return bucket.Put([]byte(id), []byte(l.String()))
+	})
 }
 
 func (s *Server) Start() error {
-	var (
-		err error
-	)
-
 	log.Printf("web: starting search index")
-	s.index, err = bleve.NewMemOnly(bleve.NewIndexMapping())
-	if err != nil {
-		return fmt.Errorf("could not start search: %s", err)
-	}
 
 	mux := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		html := `
+		html := fmt.Sprintf(`
 		<html>
 		<head>
 			<link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 		</head>
 		<body>
-		<div class="container">`
+		<div class="container">
+		<form type="GET" action="/">
+			<div class="form-group row">
+				<label for="q" class="col-sm-2 col-form-label">Query</label>
+				<input type="text" id="q" name="q" value="%s" class="col-sm-9">
+			</div>
+		</form>
+		`, html.EscapeString(r.URL.Query().Get("q")))
 
 		query := bleve.NewQueryStringQuery(r.URL.Query().Get("q"))
 		search := bleve.NewSearchRequest(query)
-		search.Highlight = bleve.NewHighlightWithStyle("html")
-		search.Highlight.AddField("Message")
+		search.Size = 1000
+		search.SortBy([]string{"Timestamp"})
 
-		results, err := s.index.Search(search)
+		result, err := s.index.Search(search)
 		if err != nil {
 			html += fmt.Sprintf("Something went wrong: %s", err)
 		} else {
-			html += fmt.Sprint(results)
+			s.db.View(func(tx *bolt.Tx) error {
+				bucket := tx.Bucket([]byte("messages"))
+
+				for _, hit := range result.Hits {
+					value := bucket.Get([]byte(hit.ID))
+					html += fmt.Sprintf("<p>%s</p>", value)
+				}
+				return nil
+			})
 		}
 		html += `</div></body></html>`
 		w.Write([]byte(html))
