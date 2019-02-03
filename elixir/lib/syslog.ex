3479236c5275dd0a044daa4a9f59d8de3f19687b
JT Archie <jtarchie@gmail.com>
2019-02-03 11:19:16 -0700

    mix format

--- elixir/lib/syslog.ex
+++ elixir/lib/syslog.ex
@@ -18,21 +18,27 @@ defmodule Syslog do
     |> integer(2)
     |> ignore(string("-"))
     |> integer(2)
-  time = integer(2)
+
+  time =
+    integer(2)
     |> ignore(string(":"))
     |> integer(2)
     |> ignore(string(":"))
     |> integer(2)
     |> optional(string("Z"))
+
   datetime = date |> ignore(string("T")) |> concat(time) |> tag(:datetime)
 
-  prival  = integer(min: 1, max: 3) |> tag(:prival)
-  pri     = string("<") |> concat(prival) |> string(">")
-  version = ascii_char([?1..?9])
+  prival = integer(min: 1, max: 3) |> tag(:prival)
+  pri = string("<") |> concat(prival) |> string(">")
+
+  version =
+    ascii_char([?1..?9])
     |> optional(ascii_char([?0..?9]))
     |> optional(ascii_char([?0..?9]))
     |> tag(:version)
-  defparsec :message, pri |> concat(version) |> string(" ") |> concat(datetime)
+
+  defparsec(:message, pri |> concat(version) |> string(" ") |> concat(datetime))
 
   def parse(msg) do
     {:ok, val, _, _, _, _} = message(msg)
@@ -42,33 +48,34 @@ defmodule Syslog do
   end
 
   defp parse1(log, [{:version, version} | p]) do
-    log = %{log | version: :erlang.list_to_integer(version) }
+    log = %{log | version: :erlang.list_to_integer(version)}
     parse1(log, p)
   end
 
   defp parse1(log, [{:prival, [prival]} | p]) do
-    log = %{log |
-      severity: prival &&& 7,
-      facility: prival >>> 3,
-      priority: prival
-    }
+    log = %{log | severity: prival &&& 7, facility: prival >>> 3, priority: prival}
     parse1(log, p)
   end
 
   defp parse1(log, [{:datetime, datetime} | p]) do
     [year, month, day, hour, minute, second] = datetime
-    log = %{log | timestamp: %DateTime{
-      year: year,
-      month: month,
-      day: day,
-      hour: hour,
-      minute: minute,
-      second: second,
-      time_zone: "Etc/UTC",
-      zone_abbr: "UTC",
-      utc_offset: 0,
-      std_offset: 0
-    }}
+
+    log = %{
+      log
+      | timestamp: %DateTime{
+          year: year,
+          month: month,
+          day: day,
+          hour: hour,
+          minute: minute,
+          second: second,
+          time_zone: "Etc/UTC",
+          zone_abbr: "UTC",
+          utc_offset: 0,
+          std_offset: 0
+        }
+    }
+
     parse1(log, p)
   end
 
--- elixir/test/syslog_test.exs
+++ elixir/test/syslog_test.exs
@@ -2,7 +2,8 @@ defmodule SyslogTest do
   use ExUnit.Case
   doctest Syslog
 
-  @valid_message ~s(<34>1 2003-10-11T22:14:15.003Z)# mymachine.example.com su 12345 98765 [exampleSDID@32473 iut="3" eventSource="Application" eventID="1011"] 'su root' failed for lonvick on /dev/pts/8)
+  # mymachine.example.com su 12345 98765 [exampleSDID@32473 iut="3" eventSource="Application" eventID="1011"] 'su root' failed for lonvick on /dev/pts/8)
+  @valid_message ~s(<34>1 2003-10-11T22:14:15.003Z)
 
   test "parses a valid message" do
     {_, offset, err} = Syslog.parse(@valid_message)
@@ -24,17 +25,18 @@ defmodule SyslogTest do
 
   test "parses timestamp" do
     {log, _, _} = Syslog.parse(@valid_message)
+
     assert log.timestamp == %DateTime{
-        year: 2003,
-        month: 10,
-        day: 11,
-        hour:  22,
-        minute: 14,
-        second: 15,
-        utc_offset: 0,
-        time_zone: "Etc/UTC",
-        zone_abbr: "UTC",
-        std_offset: 0,
-      }
+             year: 2003,
+             month: 10,
+             day: 11,
+             hour: 22,
+             minute: 14,
+             second: 15,
+             utc_offset: 0,
+             time_zone: "Etc/UTC",
+             zone_abbr: "UTC",
+             std_offset: 0
+           }
   end
 end
